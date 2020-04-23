1dc90f1f0ca6f839c71ffee75dfd9a5258a6cc36
Caine Jette <cainejette@github.com222>
2020-04-23 16:30:52 -0700

    Fix install

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ git config --global user.name "Caine Jette"
 git config --global user.email "cainejette@github.com"
 git config --global push.default current
 
-ln -s bash/.bashrc ~
+dotdir=$(cd $(dirname $0) && pwd)
+
+for filename in $(find $dotdir -name "*.symlink"); do
+  ln -sf $filename ~/.$(basename $filename | sed "s/\.symlink//")
+done
